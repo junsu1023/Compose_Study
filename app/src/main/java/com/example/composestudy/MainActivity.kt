8db78f85ad2950e10b9a782989f7eabd2c356a95
junsu1023 <12jangdokg55@gmail.com>
2023-04-03 19:17:38 +0900

    compose-state study

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.composestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +25,8 @@ class MainActivity : ComponentActivity() {
             ComposeStudyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.wrapContentSize(),
+                    color = MaterialTheme.colorScheme.secondary
                 ) {
                     Greeting("Android")
                 }
@@ -31,13 +37,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val expanded = remember { mutableStateOf(false) }
+
+    Surface(
+        modifier = Modifier.padding(24.dp)
+    )
+    {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .border(1.dp, Color.Black)
+        ) {
+            Column(Modifier.padding(3.dp)) {
+                Text(text = "Hello, ")
+                Text(name)
+            }
+            ElevatedButton(
+                onClick = { expanded.value = ! expanded.value }
+            ) {
+                Text(if(expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        Greeting("Android")
+        Greeting("Compose")
     }
 }
